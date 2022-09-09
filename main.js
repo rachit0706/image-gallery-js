b1eb747408fd1d8a40f14aaff27dfbf112e0ae21
rachit0706 <rachit0706@gmail.com>
2022-09-10 00:41:13 +0530

    Completed the project

--- main.js
+++ main.js
@@ -1,18 +1,31 @@
-const displayedImage = document.querySelector('.displayed-img');
-const thumbBar = document.querySelector('.thumb-bar');
+const displayedImage = document.querySelector(".displayed-img");
+const thumbBar = document.querySelector(".thumb-bar");
 
-const btn = document.querySelector('button');
-const overlay = document.querySelector('.overlay');
+const btn = document.querySelector("button");
+const overlay = document.querySelector(".overlay");
 
 /* Declaring the array of image filenames */
-
-/* Declaring the alternative text for each image file */
+const imageArr = ["pic1.jpg", "pic2.jpg", "pic3.jpg", "pic4.jpg", "pic5.jpg"];
 
 /* Looping through images */
+imageArr.forEach((image) => {
+  const newImage = document.createElement("img");
+  newImage.setAttribute("src", `images/${image}`);
+  newImage.setAttribute("alt", "");
+  thumbBar.appendChild(newImage);
+});
 
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-newImage.setAttribute('alt', xxx);
-thumbBar.appendChild(newImage);
+thumbBar.addEventListener('click', (e) => {
+    displayedImage.src = e.target.src;
+});
 
 /* Wiring up the Darken/Lighten button */
+btn.addEventListener("click", () => {
+  if (btn.textContent === "Darken") {
+    btn.textContent = "Lighten";
+    overlay.style.backgroundColor = "rgba(0,0,0,0.5)";
+  } else {
+    btn.textContent = "Darken";
+    overlay.style.backgroundColor = "rgba(0,0,0,0)";
+  }
+});
